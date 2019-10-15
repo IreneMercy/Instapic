a81e4fcd223dda4ea198f8c941e7c64175585d64
IreneMercy <irenemercy700@gmail.com>
2019-10-15 08:42:21 +0300

    add profile function to update profile
    add comment function to enable user make comments on the application
    
    add create_post function to enable user create post

--- Pics/views.py
+++ Pics/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import RegisterForm
+from .forms import *
 from .emails import send_welcome_email
-from .models import User, Profile
+from .models import User, Profile, Post, Comment
 
-def home(request):
-    return render(request,'index.html')
 
 def registration(request):
     if request.method == 'POST':
@@ -28,10 +26,73 @@ def registration(request):
     }
     return render(request, 'users/register.html', context)
 
+
+@login_required
+def post(request):
+    posts = Post.objects.all()
+    comments = Comment.objects.all()
+    comment_form = CommentForm()
+    context = {
+        "posts":posts,
+        "comment_form":comment_form,
+        "comments":comments
+    }
+    return render(request,'posts.html', context)
+
+
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST,request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+    else:
+        form = PostForm()
+    context = {
+        "form":form,
+    }
+    return render(request, 'post_create.html', context)
+
+@login_required
+def comment(request, post_id):
+    comment_form = CommentForm()
+    post = Post.objects.filter(pk=post_id).first()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('posts')
+    else:
+        comment_form = CommentForm()
+    context = {
+        "comment_form":comment_form,
+    }
+    return render(request, 'posts.html', context)
+
 @login_required
 def profile(request):
-    profiles = Profile.objects.all()
+    posts = Post.objects.all()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been successfully updated')
+            return redirect('profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        "profiles":profiles
+    'user_form':user_form,
+    'profile_form':profile_form,
+    'posts':posts,
     }
-    return render(request,'profile.html', context)
+    return render(request, 'profile.html', context)
